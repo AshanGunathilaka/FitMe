fafbfbbeaaf83936c0c507c0aa593d7bd97e0779
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-08-29 13:47:17 +0530

    add client files

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# FitMe Virtual Dressing Room 
---
+++ client/app/(services)/api/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "https://stale-flies-knock.loca.lt";
+
+export const registerUser = async (user) => {
+  console.log(user);
+  const response = await axios.post(
+    `${BASE_URL}/api/users/register`,
+    user,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
+export const loginUser = async (user) => {
+  const response = await axios.post(
+    `${BASE_URL}/api/users/login`,
+    user,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
+};
